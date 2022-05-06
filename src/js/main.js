317378c24d6e77b1f325fd7ddf8e7b43428f6a81
Korben-Dallas <borozan.eugen@gmail.com>
2022-05-06 13:04:48 +0300

    header + news

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,47 @@
 const burger = document.querySelector('.news-burger');
+const logo = document.querySelector('.header-logo');
+const button = document.querySelector('.button');
+const news = document.querySelector('.news');
+const header = document.querySelector('.header');
 
 burger.addEventListener('click', (e) => {
 
     //add animation to burger closes/open
-    burger.classList.toggle('news-burger--is-open') 
+    burger.classList.toggle('news-burger--is-open');
+    news.classList.toggle('news--is-open');
+    logo.classList.toggle('invert');
+    button.classList.toggle('invert');
+    burger.classList.toggle('invert');
     
+
+    
+});
+
+
+
+// News Slider
+
+const swiper = new Swiper('.news-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:3,
+    spaceBetween: 20,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-arrow--right',
+      prevEl: '.slider-arrow--left',
+    },
+  
+});
+
+
+
+//Gsap Animation 
+gsap.from("news--is-open", {
+      opacity: 0, 
+      visibility:0,
+      height: 0,
+      duration: 1
 });
