978fefb792ce1f14cc4131db7d47195208913981
Korben-Dallas-React <borozan.eugen@gmail.com>
2022-05-09 09:02:01 +0300

    rooms

--- src/js/main.js
+++ src/js/main.js
@@ -20,10 +20,24 @@ burger.addEventListener('click', (e) => {
 });
 
 
+//Room Hover Card
+const roomPost  = document.querySelectorAll('.rooms-post');
+
+roomPost.forEach( (e) => {
+  e.addEventListener('mouseenter', (e)=> {
+    let element = e.currentTarget;
+    let url = element.dataset.bgurl;
+    element.style.setProperty('background-image', 'url(' + url + ')')
+  });
+  e.addEventListener('mouseout', (e)=> {
+    let element = e.currentTarget;
+    element.style.removeProperty('background-image')
+  });
+});
 
 // News Slider
 
-const swiper = new Swiper('.news-slider', {
+const newsSlider = new Swiper('.news-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
@@ -58,4 +72,42 @@ const swiper = new Swiper('.news-slider', {
       }
     }
   
+});
+
+// Rooms Slider
+
+const roomSlider = new Swiper('.rooms-slider', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.room-slider-arrow--right',
+    prevEl: '.room-slider-arrow--left',
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+
+    },
+    // when window width is >= 640px
+    1100: {
+      slidesPerView: 2,
+
+    }
+  }
+
 });
