3c3d5a7b66207bf700b80a83076511489e22bd30
Korben-Dallas-React <borozan.eugen@gmail.com>
2022-06-13 06:00:23 +0300

    дд

--- gulpfile.js
+++ gulpfile.js
@@ -48,26 +48,26 @@ const fonts = () =>{
 //------------------------------------------------------------------------
 
 //Auto add fonts to mixins
-// const fontsStyle = (done) => {
-// 	let file_content = fs.readFileSync(srcFonts);
+const fontsStyle = (done) => {
+	let file_content = fs.readFileSync(srcFonts);
 
-// 	fs.writeFile(srcFonts, '', cb);
-// 	fs.readdir(appFonts, function (err, items) {
-// 		if (items) {
-// 			let c_fontname;
-// 			for (var i = 0; i < items.length; i++) {
-// 				let fontname = items[i].split('.');
-// 				fontname = fontname[0];
-// 				if (c_fontname != fontname) {
-// 					fs.appendFile(srcFonts, '@include font-face("' + fontname + '", "' + fontname + '", 400);\r\n', cb);
-// 				}
-// 				c_fontname = fontname;
-// 			}
-// 		}
-// 	})
+	fs.writeFile(srcFonts, '', cb);
+	fs.readdir(appFonts, function (err, items) {
+		if (items) {
+			let c_fontname;
+			for (var i = 0; i < items.length; i++) {
+				let fontname = items[i].split('.');
+				fontname = fontname[0];
+				if (c_fontname != fontname) {
+					fs.appendFile(srcFonts, '@include font-face("' + fontname + '", "' + fontname + '", 400);\r\n', cb);
+				}
+				c_fontname = fontname;
+			}
+		}
+	})
 
-// 	done();
-// }
+	done();
+}
 //Auto add fonts to mixins
 
 //---------------------------------------------------------------------
