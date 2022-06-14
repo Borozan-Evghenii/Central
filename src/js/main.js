160a7f528f39619b2db8aff979c4bb66774a0f56
Korben-Dallas-React <borozan.eugen@gmail.com>
2022-06-15 00:06:03 +0300

    mobile

--- src/js/main.js
+++ src/js/main.js
@@ -48,6 +48,25 @@ const experienceSlider = new Swiper('.experience-slider', {
     delay: 1000,
   },
 
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+
 });
 
 
